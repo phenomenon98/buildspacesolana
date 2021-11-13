180afc62ff376f2ef558df8cf60a099e03302fbb
phenomenon98 <rmenon1998@gmail.com>
2021-11-13 18:50:39 +0530

    finished course

--- tests/buildspacesolana.js
+++ tests/buildspacesolana.js
@@ -1,27 +1,50 @@
 const anchor = require('@project-serum/anchor');
 
-// describe('buildspacesolana', () => {
-
-//   // Configure the client to use the local cluster.
-//   anchor.setProvider(anchor.Provider.env());
-
-//   it('Is initialized!', async () => {
-//     // Add your test here.
-//     const program = anchor.workspace.Buildspacesolana;
-//     const tx = await program.rpc.initialize();
-//     console.log("Your transaction signature", tx);
-//   });
-// });
-
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Buildspacesolana;
-  const tx = await program.rpc.startStuffOff();
+	
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Call add_gif!
+  await program.rpc.addGif("https://lh3.googleusercontent.com/HOXT4FC1kp6pWQOcqlC-Sz3VeVO7SpPrU8djdxZG0IoEAs3ilt1YwJTp3aNIchpo_qfwKycr9yMGzxLdRZ_dmk11=w600", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  console.log('👀 GIF List', account.gifList)
+
 }
 
 const runMain = async () => {
